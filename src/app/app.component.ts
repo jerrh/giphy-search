33086b8b048905684619cfea4aeec288356db37a
Jerri Ho <jerrisays@yahoo.com>
2018-05-09 17:48:19 -0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Welcome to ngGiphy!';
+  link = 'http://api.giphy.com/v1/gifs/search?api_key=API+KEY&q=';
+  http: Http;
+  giphies = [];
+  
+  constructor(http: Http) {
+    this.http = http;
+  }
+
+  performSearch(searchTerm: HTMLInputElement): void {
+    var apiLink = this.link + searchTerm.value;
+
+    this.http.request(apiLink)
+      .subscribe((res: Response) => {
+        this.giphies = res.json().data;
+        console.log(res.json());
+      });
+  }
 }
